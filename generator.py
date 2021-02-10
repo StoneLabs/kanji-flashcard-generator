66fe53a54d3b27bda914cded5f62da41bcc393cd
StoneLabs <git@sobadev.com>
2021-02-10 19:31:02 +0100

    Add pdf ouput

--- generator.py
+++ generator.py
@@ -4,6 +4,8 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
+from fpdf import FPDF
+
 from rich.table import Table
 from rich.console import Console
 console = Console()
@@ -61,45 +63,55 @@ for grade in range(grade_min, grade_max + 1):
     gradeFolder = os.path.join(outputFolder, "grade_" + str(grade))
     create_folder_or_warn(gradeFolder)
 
+    # Create PDF file
+    pdf = FPDF(unit = "pt", format = [template_width, template_height])
+
     with open("data/grade" + str(grade) + ".csv", encoding="utf8") as c_file:
-            for line in parse_csv(c_file.read(), name=("Grade " + str(grade))):
-                index = line[0]
-                kanji = line[1]
-                strok = line[2]
-                trans = line[3]
-                oread = line[4]
-                kread = line[5]
-                outputFile = os.path.join(gradeFolder, "front_" + str(index) + ".jpg")
+        for line in parse_csv(c_file.read(), name=("Grade " + str(grade))):
+            index = line[0]
+            kanji = line[1]
+            strok = line[2]
+            trans = line[3]
+            oread = line[4]
+            kread = line[5]
+            outputFile = os.path.join(gradeFolder, "front_" + str(index) + ".jpg")
 
-                img = Image.open("template.jpg")
-                draw = ImageDraw.Draw(img)
+            img = Image.open("template.jpg")
+            draw = ImageDraw.Draw(img)
 
-                # Translation
-                w, h = draw.textsize(str(trans).replace(";", " ").split(" ", 1)[0], font=font_oktop)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.19 - h/2), str(trans).replace(";", " ").split(" ", 1)[0], (0, 0, 0), font=font_oktop)
+            # Translation
+            w, h = draw.textsize(str(trans).replace(";", " ").split(" ", 1)[0], font=font_oktop)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.19 - h/2), str(trans).replace(";", " ").split(" ", 1)[0], (0, 0, 0), font=font_oktop)
 
-                # Kanji
-                w, h = draw.textsize(str(kanji), font=font_kanji)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.4 - h/2), str(kanji), (0, 0, 0), font=font_kanji)
+            # Kanji
+            w, h = draw.textsize(str(kanji), font=font_kanji)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.4 - h/2), str(kanji), (0, 0, 0), font=font_kanji)
 
-                # On
-                w, h = draw.textsize("- On -", font=font_oktop)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.6 - h/2), "- On -", (0, 0, 0), font=font_oktop)
+            # On
+            w, h = draw.textsize("- On -", font=font_oktop)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.6 - h/2), "- On -", (0, 0, 0), font=font_oktop)
 
-                # On Value
-                w, h = draw.textsize(oread, font=font_onkun)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.67 - h/2), oread, (0, 0, 0), font=font_onkun)
+            # On Value
+            w, h = draw.textsize(oread, font=font_onkun)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.67 - h/2), oread, (0, 0, 0), font=font_onkun)
 
-                # Kun
-                w, h = draw.textsize("- Kun -", font=font_oktop)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.75 - h/2), "- Kun -", (0, 0, 0), font=font_oktop)
+            # Kun
+            w, h = draw.textsize("- Kun -", font=font_oktop)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.75 - h/2), "- Kun -", (0, 0, 0), font=font_oktop)
 
-                # Kun Value
-                w, h = draw.textsize(kread, font=font_onkun)
-                draw.text((template_width * 0.5 - w/2, template_height * 0.82 - h/2), kread, (0, 0, 0), font=font_onkun)
+            # Kun Value
+            w, h = draw.textsize(kread, font=font_onkun)
+            draw.text((template_width * 0.5 - w/2, template_height * 0.82 - h/2), kread, (0, 0, 0), font=font_onkun)
 
-                # Index
-                w, h = draw.textsize("Kanji #" + str(index), font=font_index)
-                draw.text((template_width - w, template_height - h), "Kanji #" + str(index), (0, 0, 0), font=font_index)
+            # Index
+            w, h = draw.textsize("Kanji #" + str(index), font=font_index)
+            draw.text((template_width - w, template_height - h), "Kanji #" + str(index), (0, 0, 0), font=font_index)
 
-                img.save(outputFile)        
+            img.save(outputFile)
+
+            # Add to pdf
+            pdf.add_page()
+            pdf.image(outputFile, 0, 0)
+    
+    # Output pdf
+    pdf.output(os.path.join(outputFolder, "grade_" + str(grade) + ".pdf"), "F")
